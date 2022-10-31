ae359989ff3d1678f4f94df3815f84b6a408e47b
David Benham <drbenham@ucsd.edu>
2022-10-30 21:08:14 -0400

    package update and upload

--- helm-charts/build.sh
+++ helm-charts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cr package drupal-helm
-cr index .repo
 cp .cr-index/index.yaml ./
 cr upload drupal-helm
+git add -A ; git commit -m 'package update and upload'; git push
+sleep 120
+cr index .repo
 git add -A ; git commit -m 'package update and upload'; git push
 helm repo update
